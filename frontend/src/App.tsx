e1a70d482a06f411603371e4e8c1d8f2e3115b01
marijaag <marijagaric3007@gmail.com>
2026-02-03 17:10:02 +0100

    Login stranica

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home.tsx';
+import Login from './pages/Login.tsx'; 
 
 function App() {
-  return <Home />;
+  return (
+  <Router>
+      <Routes>
+        {/* Početna stranica */}
+        <Route path="/" element={<Home />} />
+        
+        {/* Stranica za login */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
