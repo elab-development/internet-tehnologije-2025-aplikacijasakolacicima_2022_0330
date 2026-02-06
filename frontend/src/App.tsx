2186e16fbe26fbf135d6648fb8b8c92ddb53cda4
marijaag <marijagaric3007@gmail.com>
2026-02-06 16:37:12 +0100

    ZapamtiMe funkcionalnost - kolacici

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { useEffect } from 'react';
+import axios from 'axios';
 import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx'; 
+import Logout from './pages/Logout.tsx'; 
+
 
 function App() {
+
+  useEffect(() => {
+    const checkUser = async () => {
+        try {
+            const res = await axios.get('/api/user');
+            console.log("Korisnik je ulogovan:", res.data.user);
+        } catch (err) {
+            console.log("Korisnik nije ulogovan");
+            localStorage.removeItem('user_name');
+        }
+    };
+    checkUser();
+}, []);
+
   return (
   <Router>
       <Routes>
-        {/* Početna stranica */}
         <Route path="/" element={<Home />} />
-        
-        {/* Stranica za login */}
         <Route path="/login" element={<Login />} />
+
+        <Route path="/logout-test" element={<Logout />} />
       </Routes>
     </Router>
   );
