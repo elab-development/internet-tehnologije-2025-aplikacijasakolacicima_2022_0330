9b0253aff8678077d5e823d29fee11efe41c8acf
jellllee <djokicjelena268@gmail.com>
2026-03-08 23:08:38 +0100

    Auto migrate and seed on deploy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,20 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if (User::count() == 0) {
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@test.com',
+                'password' => bcrypt('password123'),
+                'role' => 'admin',
+            ]);
+        }
     }
 }
