6fc73b68209411b0d984fa5475c1dab92e5f3d16
jellllee <djokicjelena268@gmail.com>
2026-03-10 22:10:30 +0100

    Fix axios baseURL for production

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+/****/
+const isProduction = window.location.hostname !== 'localhost';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api', 
+  //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api', 
+  baseURL: isProduction 
+  ? 'https://kolacici-backend.onrender.com/api' 
+  : 'http://localhost:8000/api',
   withCredentials: true, 
   /*headers: {
     'Accept': 'application/json',
