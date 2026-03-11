c379086efb954f2911d743ba081916c3a3831d91
jellllee <djokicjelena268@gmail.com>
2026-03-11 13:55:03 +0100

    izmene u axios

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 /****/
-const isProduction = window.location.hostname !== 'localhost';
+//const isProduction = window.location.hostname !== 'localhost';
 
 const api = axios.create({
-  //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api', 
-  baseURL: isProduction 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api', 
+ /* baseURL: isProduction 
   ? 'https://kolacici-backend.onrender.com/api' 
-  : 'http://localhost:8000/api',
+  : 'http://localhost:8000/api', */
   withCredentials: true, 
   /*headers: {
     'Accept': 'application/json',
